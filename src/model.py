8af4298d9e85a7a36b66325c651d7c5f03847108
Adi Faintuch <afaintuc@uci.edu>
2020-02-10 22:05:30 -0800

    cards successfully change when a set is found

--- src/model.py
+++ src/model.py
@@ -2,7 +2,7 @@
 from random import shuffle
 from collections import namedtuple
 
-Card = namedtuple('Card', 'color, shape, number, shading, position, x, y')
+Card = namedtuple('Card', 'color, shape, number, shading, position')
 
 #Card = namedtuple('Card', 'color, shape, number, shading, x, y')
 
@@ -20,7 +20,7 @@ def create_deck():
 		for s in shapes:
 			for n in number:
 				for sh in shading:
-					new_card = Card(c, s, n, sh, card_pos, 0, 0)
+					new_card = Card(c, s, n, sh, card_pos)
 					#new_card = Card(c, s, n, sh, 0, 0)
 					deck.append(new_card)
 					card_pos += 1
@@ -34,22 +34,28 @@ def create_initial_twelve_cards(deck):
 		used_cards.append(deck[i])
 	return used_cards
 
-<<<<<<< HEAD
 def add_three_new_cards(deck, displayed_cards, list_of_index):
 	'''adds 3 new cards to the displayed_cards list'''
 	for i in range(3):
+		print("testing0 ", i, "\n")
+		print("testing1 ", list_of_index[i], "\n")
+		print("testing2 ", deck[i], "\n")
 		displayed_cards[list_of_index[i]] = deck[i]
-=======
-def add_three_new_cards(deck, displayed_cards):
-	'''adds 3 new cards to the displayed_cards list'''
+
+def add_three_new_cards(deck, displayed_cards, list_of_index):
+	'''adds 3 new cards to the displayed_cards list and removes them from deck'''
 	for i in range(3):
-		displayed_cards.append(deck[i])
->>>>>>> 15c3f937b41cd6eec21301a98617babf2fd2f621
+		displayed_cards[list_of_index[i]] = deck[i]
+	for j in range(3):
+		deck.remove(displayed_cards[list_of_index[j]])
+	# for j in range(3):
+	# 	deck.remove(displayed_cards[list_of_index[i]])
 
 def remove_used_cards_from_deck(deck, used_cards):
 	'''removed the used cards from the deck'''
 	for card in used_cards:
 		deck.remove(card)
+
 
 def check_for_set(card_dict):
 	'''checks to see if a dictionary of 3 cards is a set'''
